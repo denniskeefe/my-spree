f66cc7cae6a580d0592af45d9861744050eb05b0
Dennis Keefe <dkeefe75@gmail.com>
2013-04-04 00:01:04 -0500

    predeployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mystore::Application.config.secret_token = '3ab09525255c6a5a07ec6cadacd259971cd46138f9a11c90bdfbd3ef91bc7938f040688e1697a110db14a576435d35c04b6f8cdf8a9104d4a2fb28cd79eefe10'
